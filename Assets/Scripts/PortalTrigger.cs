2b8c83e6d6142251424a3312c2f105c7cb741a6f
critical-matt <mais4550@colorado.edu>
2018-02-12 19:26:50 -0700

    added intro scene

---
+++ Assets/Scripts/PortalTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTrigger : MonoBehaviour {
+
+	void Update {
+		var portal1;
+
+
+		function OnTriggerEnter (other Collider) {
+			//Check to see if a player entered the gate, rather than some space debris.
+			if (other.gameObject.CompareTag("Player")) {
+				Application.LoadLevel("portal1");
+			}
+		}
+}
+
+}
+
+	
+
+
+
+
