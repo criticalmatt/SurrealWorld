ad476f8f6a58ca00b54b4e40960ec555240cd5a2
critical-matt <mais4550@colorado.edu>
2018-02-12 23:10:59 -0700

    portal

---
+++ Assets/Scripts/Portal2Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal2Script : MonoBehaviour {
+
+	void Start() {
+		Debug.Log ("script started");
+		Debug.Log (gameObject.name);
+	}
+
+	void Update (){
+	}
+
+	void OnTriggerEnter (Collider col){
+		//void OnCollisionEnter (Collider col){
+
+		SceneManager.LoadScene ("golconda", LoadSceneMode.Additive);
+		Debug.Log ("entered scene");
+
+
+		//Debug.Log ("outside if");
+	}
+}
---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour {
+
+	void Start() {
+		Debug.Log ("script started");
+		Debug.Log (gameObject.name);
+	}
+
+	void Update (){
+	}
+
+	void OnTriggerEnter (Collider col){
+	//void OnCollisionEnter (Collider col){
+
+			SceneManager.LoadScene ("ScrewAndStump", LoadSceneMode.Additive);
+			Debug.Log ("entered scene");
+
+
+		//Debug.Log ("outside if");
+	}
+}
